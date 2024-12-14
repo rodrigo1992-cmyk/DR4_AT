f877aaa10c0627d286885fee1ae5cab3c32534d2
rodrigo1992-cmyk <145853715+rodrigo1992-cmyk@users.noreply.github.com>
2024-12-14 00:08:11 -0300

    CONCLUÍDO

--- app/dataprep.py
+++ app/dataprep.py
@@ -2,6 +2,11 @@ import requests
 import pandas as pd
 import PyPDF2
 from io import BytesIO
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+import time
+import json
 
 def get_deputados():
     '''
@@ -125,10 +130,167 @@ def get_proposicoes():
     return df
         
 
+def criar_chunks(text, window, overlap):
+    '''
+    Recebe uma lista de frases e retorna uma lista de chunks.
+    '''
+    
+    chunks = []
+    for i in range(0, len(text), window - overlap):
+        chunk = text[i:i + window] 
+        chunks.append(" ".join(chunk))
+        if i + window >= len(text):
+            break
+    return chunks
 
+
+def criar_chunks_por_proposicao(df):
+    '''
+    Recebe um DataFrame com proposições e retorna uma lista de dicionários, onde cada dicionário contém o ID da proposição e uma lista de chunks de texto.
+    '''
+
+    df = df.groupby('codTema').head(10)
+
+    list_chunksPorProposicao = []
+    #iterar em cada proposição
+    for i, row in df.iterrows():
+        texto = row['texto'].replace('\n', ' ')
+        #retirar espaços duplos
+        texto = ' '.join(row['texto'].split())
+        list_frases = texto.split('.')
+        #remover posições vazias da lista ou que sejam apenas um espaço em branco
+        list_frases = [frase for frase in list_frases if frase != '' and frase != ' ']
+
+        #Devido a estrutura do documento, ficaram muitas linhas com apenas um número ou palavra solta. Neste caso, irei juntar com a linha anterior.
+        list_frasesNew = []
+
+        for i in range(0,len(list_frases)-1):
+            if i == 0 or len(list_frases[i].split()) > 3:
+                list_frasesNew.append(list_frases[i])
+            # Juntar com o item anterior se tiver até 3 palavras.
+            else:
+                list_frasesNew[-1] += ' ' + list_frases[i]
+
+            chunks = criar_chunks(list_frasesNew, 10, 3)
         
+        new_chunk = {'idProp': row['idProp'], 'chunks': chunks}
+        list_chunksPorProposicao.append(new_chunk)
+        
+        return list_chunksPorProposicao
 
 
+def sumarizar_chunks(list_chunksPorProposicao):
+    '''
+    Recebe uma lista de dicionários com os chunks de texto de cada proposição e retorna um dicionário com os resumos de cada chunk.
+    '''
+
+    dict_summaries = {}
+
+    prompt = f"""
+    Você é um reporter político.
+    Você receberá um trecho de uma proposta legislativa.
+    Seu objetivo é resumir o texto em uma única sentença.
+
+    Trecho da proposta:
+
+    """
+
+    genai.configure(api_key=os.getenv('GEMINI_KEY'))
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    model.temperature = 0.3
 
 
-            
+    for proposta in list_chunksPorProposicao:
+        chunks = proposta['chunks']
+        idProp = proposta['idProp']
+
+        if idProp not in dict_summaries:
+            dict_summaries[idProp] = []
+
+        for chunk in chunks:
+            response = model.generate_content(prompt + chunk)
+            dict_summaries[idProp].append(response.text)
+
+            #Há cota de 15 chamadas por minuto, então tive de adicionar um bom tempo entre chamadas para não exceder.
+            time.sleep(4.5)
+
+    return dict_summaries
+
+def sumarizacao_final(dict_summaries):
+    '''
+    Recebe um dicionário com os resumos dos chunks, faz o resumo final de cada proposicao e guarda o resultado em um arquivo JSON.
+    '''
+    
+    df_summaries = pd.DataFrame(columns=['idProp', 'summary'])
+
+    prompt = f"""
+    Você é um reporter político.
+    Você receberá resumos de trechos de uma proposta legislativas.
+    Seu objetivo é condensar de em um único texto, as informações mais relevantes da proposta.
+
+    Resumos dos trechos:
+
+    """
+
+    genai.configure(api_key=os.getenv('GEMINI_KEY'))
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    model.temperature = 0.3
+
+
+    for id in dict_summaries:
+        chunks = dict_summaries[id]
+
+        response = model.generate_content(prompt + ' '.join(chunks))
+        df_iter = pd.DataFrame({'idProp': id, 'summary': response.text}, index=[0])
+        df_summaries = pd.concat([df_summaries, pd.DataFrame(df_iter)])
+        
+        #Há cota de 15 chamadas por minuto, então tive de adicionar um bom tempo entre chamadas para não exceder.
+        time.sleep(4.5)
+
+    df_summaries.to_json(r'C:\Users\RodrigoPintoMesquita\Documents\GitHub\DR4_AT\data\sumarizacao_proposicoes.json', orient='records', indent=4, force_ascii=False)
+
+
+def unificar_datasets():
+    '''
+    Unifica os datasets de deputados, despesas e proposições em um único arquivo JSON.
+    '''
+
+    #Cada Json está em um formato diferente, então está sendo necessário fazer a conversão manualmente deixando em um mesmo formato de lista.
+
+    #Importando arquivo de distribuição de deputados e transformando em lista
+    with open(r'C:\Users\RodrigoPintoMesquita\Documents\GitHub\DR4_AT\data\insights_distribuicao_deputados.json', 'r', encoding='utf-8') as file:
+        texto = json.load(file)
+        texto = texto['text'].replace('*', '')
+
+    lista_temp = [paragrafo.strip() for paragrafo in texto.split('\n\n') if paragrafo.strip()]
+
+    listas = []
+    for item in lista_temp:
+        listas.append(item)
+
+    #Importando arquivo de distribuição de despesas e transformando em lista
+    with open(r'C:\Users\RodrigoPintoMesquita\Documents\GitHub\DR4_AT\data\insights_despesas_deputados.json', 'r', encoding='utf-8') as file:
+        texto = json.load(file)
+    lista_temp = []
+    for item in texto:
+        lista_temp.append(list(item.values()))
+
+    lista_temp = [': '.join(sublista) for sublista in lista_temp]
+    for item in lista_temp:
+        listas.append(item)
+
+    #Importando arquivo de proposições e transformando em lista
+    with open(r'C:\Users\RodrigoPintoMesquita\Documents\GitHub\DR4_AT\data\sumarizacao_proposicoes.json', 'r', encoding='utf-8') as file:
+        texto = json.load(file)
+
+    lista_temp = []
+    for item in texto:
+        lista_temp.append(list(item.values()))
+
+    lista_temp = [': '.join(map(str, sublista)) for sublista in lista_temp]
+    for item in lista_temp:
+        listas.append(item)
+
+    #Exportar para arquivo json
+    with open(r'C:\Users\RodrigoPintoMesquita\Documents\GitHub\DR4_AT\data\input_embedding.json', 'w', encoding='utf-8') as file:
+        json.dump(listas, file, ensure_ascii=False, indent=4)
